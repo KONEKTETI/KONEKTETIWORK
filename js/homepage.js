7e9905429774cd1b4c8b461ce50aff0a07b44aff
KONEKTETI <116629844+KONEKTETI@users.noreply.github.com>
2022-11-11 06:45:50 +0700

    Add files via upload

---
+++ js/homepage.js
@@ -0,0 +1,116 @@
+swal.fire("Please Wait")
+swal.showLoading()
+
+firebase.auth().onAuthStateChanged(async (user) => {
+    if(user){
+        console.log("user ada") // del
+        console.log(user.email.slice(-9))
+        if(user.email.slice(-9) != "ugm.ac.id"){
+            console.log("akunnya bukan ugm") // del
+            // alert("bukan email ugm, silakan login ulang");
+            swal.close()
+            Swal.fire({
+                icon: 'error',
+                title: 'Bukan email  UGM',
+                text: 'Mohon untuk menggunakan email UGM',
+            }).then(()=>{
+                firebase.auth().signOut().then(()=> {
+                    console.log('logged out')
+                }).catch((error) => {
+                    console.log(error.message)
+                })
+            })
+            swal.stopLoading()
+        } else {
+            console.log("akun ugm") // del
+            console.log("baca database dulu") // del
+            console.log("executed")
+            firebase.database().ref('accounts/').orderByChild('0').equalTo(user.email).once('value', function (snapshot) {
+                console.log("database dapet") // del
+                var value = snapshot.val();
+                console.log(value);
+                if(value){
+                    console.log("orangnya udah terdaftar") // del
+                    firebase.database().ref('encode/').orderByChild('0').equalTo(user.email).once('value', function (snapshot) {
+                        var value = snapshot.val();
+                        if(value){
+                            console.log("encode udah") // del
+                            snapshot.forEach(function(child) {
+                                token = child.val()[1]
+                            });
+                            firebase.database().ref('vote/').orderByChild('0').equalTo(token).once('value', function (snapshot) {
+                                var value = snapshot.val();
+                                if(value){
+                                    console.log("udah vote") // del
+                                    window.location.href = "pages/thankyou.html"
+                                } else {                    
+                                    window.location.href = "pages/vote.html"
+                                }
+                            });
+                        } else {                    
+                            window.location.href = "pages/vote.html"
+                        }
+                    });
+                } else {                   
+                    swal.close()
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Anda belum terdaftar',
+                        html: 'silakan mendaftarkan diri di <br /><a href="https://docs.google.com/forms/d/e/1FAIpQLSfNfE_2YR_ZdfPuMst8IEAZs73NUVjx8aa_IhjJxgss-1mgLQ/viewform" target="_blank">Link Ini</a>',
+                    }).then(()=>{
+                        firebase.auth().signOut().then(()=> {
+                            console.log('logged out')
+                        }).catch((error) => {
+                            console.log(error.message)
+                        })
+                    })
+                }
+            });
+            console.log("done")
+        }
+    } else {
+        swal.close()
+    }
+})
+
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 9, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer-icon").remove('fa-clock');
+    document.getElementById("countdown").innerHTML = "Vote Now";
+    document.getElementById("countdown").setAttribute('onclick', "login()");
+  }
+}, 1000);
+
+
+
+$(document).ready(function(){ 
+    $('#vote').on('click', function(e) {
+        e.preventDefault();
+        login();
+    });
+});
